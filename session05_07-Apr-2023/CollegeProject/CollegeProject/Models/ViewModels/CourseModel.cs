1c4a574167ce92d6687551f7ac9a417bb49a0468
Sajad Shafi <maliksajad004@gmail.com>
2023-04-07 15:25:53 +0530

    session05 initial

---
+++ session05_07-Apr-2023/CollegeProject/CollegeProject/Models/ViewModels/CourseModel.cs
@@ -0,0 +1,7 @@
+namespace CollegeProject.Models.ViewModels {
+    public class CourseModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CourseFee { get; set; }
+    }
+}
