9c02c52e0cb530a41888c9811687f0a365e90731
Sajad Shafi <maliksajad004@gmail.com>
2023-04-25 20:07:18 +0530

    Completed session 12

---
+++ session12_25_Apr_2023/mvcJS/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿function DeleteEmployee(target) {
+  let id = target.getAttribute("data-id");
+
+  // var myModalEl = document.getElementById('confirmDialog');
+  // var modal = bootstrap.Modal.getInstance(myModalEl)
+
+  var modal = new bootstrap.Modal(document.getElementById('confirmDialog'))
+  modal.show();
+
+  document.getElementById("confirmDelete").addEventListener('click', () => {
+    fetch("Home/DeleteEmployee?id=" + id, {
+      method: "POST"
+    })
+    .then(response => response.text())
+    .then(result => {
+      console.log("Delete: ", result)
+      let currentEmployee = document.getElementById(result);
+      currentEmployee.parentElement.removeChild(currentEmployee);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  });
+}
+
+function EditEmployee(target) {
+  let emp = target.getAttribute("data-item");
+  console.log("Edit clicked");
+}
