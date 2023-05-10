5cf2a45f7936e246fd9dd6d73e69489d0c584568
Sajad Shafi <maliksajad004@gmail.com>
2023-05-10 16:06:39 +0530

    Completed Session17

---
+++ session17_10_May_2023/AuthBasic/AuthBasic/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AuthBasic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AuthBasic.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContext) {
+            _logger = logger;
+            this._httpContext = httpContext;
+        }
+
+        public IActionResult Index() {
+            _logger.LogError("User is: {0}", _httpContext.HttpContext.Session.GetString("User"));
+
+            bool userExist = _httpContext.HttpContext.Session.GetString("User") != null;
+            _logger.LogError("User exist: {0}", userExist.ToString());
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        public IActionResult Dashboard() {
+            return Ok("This is dashboard");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ session17_10_May_2023/AuthBasic/AuthBasic/Utils/Encrypt.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace AuthBasic.Utils {
+    public class Encrypt {
+        public static string EncryptPassword(string password) {
+
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+            byte[] hash = SHA256.HashData(bytes);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
