aeb618a7f6b06384f2551bb0cfab79c17d0dd54e
Sajad Shafi <maliksajad004@gmail.com>
2023-05-12 17:25:33 +0530

    Session19

---
+++ session19_12-May-2023/TaskTracker/TaskTracker/IServices/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using TaskTracker.Models.ViewModels;
+
+namespace TaskTracker.IServices {
+    public interface IUserRepository {
+        Task<bool> LoginAsync(LoginVM model);
+        Task<bool> RegisterAsync(UserVM model);
+        Task LogoutAsync();
+    }
+}
