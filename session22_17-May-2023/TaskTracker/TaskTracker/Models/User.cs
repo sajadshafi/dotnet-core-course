75e82ac0085b0367c5edd3ccb5d97f182a34a341
Sajad Shafi <maliksajad004@gmail.com>
2023-05-17 16:28:07 +0530

    session22 session completed

---
+++ session22_17-May-2023/TaskTracker/TaskTracker/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskTracker.Models {
+    public class SystemUser : IdentityUser<Guid> {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class SystemRole : IdentityRole<Guid> {
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ session22_17-May-2023/TaskTracker/TaskTracker/Models/ViewModels/UserVM.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Models.ViewModels {
+    public class UserVM {
+
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginVM {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
